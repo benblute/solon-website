85cce76f1192a298e0143de693ffc7c1030d0728
BenBlute <benblute@gmail.com>
2022-04-02 18:09:24 -0400

    docs now opens in new tab

--- components/Header.js
+++ components/Header.js
@@ -67,7 +67,7 @@ function Menu({style}) {
     <div className="container" style={style}>
       <NavigationButton href="/" dot><strong>Home</strong></NavigationButton>
       <NavigationButton href="/nfts" dot><strong>NFTs</strong></NavigationButton>
-      <NavigationButton href="https://solon.gitbook.io/docs" dot><strong>Docs</strong></NavigationButton>
+      <a href="https://solon.gitbook.io/docs" target="_blank"><strong>Docs</strong></a>
       <Spacer width="64px" />
       <SocialMedia />
       <Spacer width="64px" />
